0814e0277dff4824b7c742ab1037062690c80fb6
Francesco Romanenghi <romanenghi@libero.it>
2011-07-08 15:07:09 +0200

    .

--- app/models/gunpla.rb
+++ app/models/gunpla.rb
@@ -1,4 +1,6 @@
 class Gunpla < ActiveRecord::Base
+  has_many :gunpla_categories
+  has_many :gunpla_series
   has_many :categories :through => :gunpla_categories
   has_many :series :through => :gunpla_series
 end
