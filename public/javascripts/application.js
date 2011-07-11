2a0b11fd724f951f9a24bb8c2d19723150980bec
Francesco Romanenghi <romanenghi@libero.it>
2011-07-11 19:16:11 +0200

    jquery ui e relativi css aggiunti

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,40 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+		var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( "#gunpla_jancode" ).autocomplete({
+			source: availableTags
+		});
+	$(".draggable").draggable({helper: 'clone'});
+	$(".droppable" ).droppable({
+			drop: function( event, ui ) {
+				$( this )
+					.addClass( "ui-state-highlight" )
+					.find( "p" )
+						.html( "Dropped!" );
+			}
+		});
+});
