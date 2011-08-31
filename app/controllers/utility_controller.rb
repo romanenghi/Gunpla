2e207c8937c1c7b3cc5c68013244014f6e74bf73
Francesco Romanenghi <romanenghi@libero.it>
2011-08-31 10:39:19 +0200

    .

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -7,9 +7,7 @@ class UtilityController < ApplicationController
   def gunplahome
     @gunplapg = Accessready.new.getproductstype("pg")
     tmp = render_to_string(:layout => false)
-    tmp2 = Iconv.iconv("ISO-8859-1//TRANSLIT","UTF-8",tmp)
-    puts tmp2
-    Accessready.new.updategunplahome(tmp2)
+    Accessready.new.updategunplahome(tmp)
   end
 end
 
