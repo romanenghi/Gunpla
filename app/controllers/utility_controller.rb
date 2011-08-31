95a06e3a4670b8aa3321de33963ec3e860027c1c
Francesco Romanenghi <romanenghi@libero.it>
2011-08-31 12:02:35 +0200

    .

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -7,7 +7,6 @@ class UtilityController < ApplicationController
   def gunplahome
     @gunplapg = Accessready.new.getproductstype("pg")
     tmp = render_to_string(:layout => false)
-    puts tmp.encode("ISO-8859-15")
     Accessready.new.updategunplahome(tmp)
   end
 end
