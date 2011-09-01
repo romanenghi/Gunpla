b7ae5d1dbd85e302a7b8999ea5a96c6f1a78553f
Francesco Romanenghi <romanenghi@libero.it>
2011-09-01 15:52:49 +0200

    .

--- app/controllers/utility_controller.rb
+++ app/controllers/utility_controller.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
 
 require 'htmlentities'
+require 'open-uri'
+require 'nokogiri'
+require 'net/ftp'
+
+
 
 class UtilityController < ApplicationController
   def index
@@ -16,12 +21,30 @@ class UtilityController < ApplicationController
 
     @gunplatypes.each do |gunplatype|
       @gunplatypelist[n] = Accessready.new.getproductstype(gunplatype.codiceinterno)
-      @gunplatypedescription[n] = {"title" => gunplatype.name,
-                                   "image" => gunplatype.image,
-                                   "description" => HTMLEntities.new.encode(gunplatype.description, :named)}
+      @gunplatypedescription[n] = {"title" => gunplatype.name.strip,
+        "image" => gunplatype.image,
+        "description" => HTMLEntities.new.encode(gunplatype.description, :named)}
       n=n+1
     end
-    Accessready.new.updategunplahome(render_to_string(:layout => false))
+    tmp = render_to_string
+    doc = Nokogiri::HTML::Document.parse(tmp)
+    # Save a string to a file.
+
+    @gunplatypes.each do |gunplatype|
+      html = doc.xpath("//*[@id='#{gunplatype.name.strip}']")
+      aFile = File.new("#{gunplatype.name.strip}.html", "w")
+      aFile.write(html)
+      aFile.close
+      ftp = Net::FTP.new('ftp.starshopbs.com')
+      ftp.passive = true
+      ftp.login("2039658@aruba.it", "n8uqrrrd")
+      ftp.chdir('/starshopbs.com/public')
+      ftp.putbinaryfile("#{gunplatype.name.strip}.html")
+      ftp.close
+    end
+       
+       html = doc.xpath("//*[@id='headergundamtype']")
+       Accessready.new.updategunplahome(html.to_s)
   end
 end
 
